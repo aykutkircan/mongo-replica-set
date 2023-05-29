4beb95a0dd32b0542c14fe3c4b8476e4a98ae077
Aykut Kırcan <aykutkircan@icloud.com>
2023-05-29 12:04:46 +0300

    fix: refactoring up.sh and down.sh

--- down.sh
+++ down.sh
@@ -2,19 +2,14 @@
 
 export COMPOSE_PROJECT_NAME=docker
 
-# Parametreleri yakala
+# Capture parameters
 ENV=$1
 options=$2
 
-# --volumes parametresine göre işlem yap
-if [[ "$options" == "--volumes" ]]; then
-
+# Process based on the --volumes option
+if [ "$options" = "--volumes" ]; then
   docker-compose --file "docker-compose-${ENV}.yaml" down --volumes
-  rm -rf ./mongo-shard-scripts/${ENV}/mongodb.key
+  rm -rf "./mongo-shard-scripts/${ENV}/mongodb.key"
 else
-
   docker-compose --file "docker-compose-${ENV}.yaml" down
-
 fi
-
-
--- up.sh
+++ up.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-## Composer project name instead of git main folder name
+# Set the Composer project name instead of git main folder name
 export COMPOSE_PROJECT_NAME=docker
 
-# Parametreleri yakala
+# Capture parameters
 ENV=$1
 options=$2
 
@@ -14,44 +14,45 @@ echo "ENV is: ${ENV}"
 echo "Option is: ${options}"
 sleep 2
 
-# --init parametresine göre işlem yap
-if [[ "$options" == "--init" ]]; then
-    # --init seçeneğiyle ilgili işlemler
-    echo "Init işlemi yapılıyor..."
+# Process based on the --init option
+if [ "$options" = "--init" ]; then
+    # Operations for the --init option
+    echo "Init operation in progress..."
     # Generate global auth key between cluster nodes
-    openssl rand -base64 756 > ./mongo-shard-scripts/${ENV}/mongodb.key
-    chmod 600 ./mongo-shard-scripts/${ENV}/mongodb.key
+    openssl rand -base64 756 > "./mongo-shard-scripts/${ENV}/mongodb.key"
+    chmod 600 "./mongo-shard-scripts/${ENV}/mongodb.key"
+    echo "chmod 600 success for './mongo-shard-scripts/${ENV}/mongodb.key' file"
+
+    sleep 3
 
     # Start the whole stack
     docker-compose --file "docker-compose-${ENV}.yaml" up -d
 
-    ## Config servers setup
+    # Config servers setup
     docker exec -it "${COMPOSE_PROJECT_NAME}-${ENV}-configserver-01-1" sh -c "mongosh --port 27017 < /mongo-configserver.init.js"
 
-    ## Shard servers setup
+    # Shard servers setup
     docker exec -it "${COMPOSE_PROJECT_NAME}-${ENV}-shard-01a-1" sh -c "mongosh --port 27018 < /mongo-shard-01.init.js"
     docker exec -it "${COMPOSE_PROJECT_NAME}-${ENV}-shard-02a-1" sh -c "mongosh --port 27019 < /mongo-shard-02.init.js"
     docker exec -it "${COMPOSE_PROJECT_NAME}-${ENV}-shard-03a-1" sh -c "mongosh --port 27020 < /mongo-shard-03.init.js"
 
-    ## Apply sharding configuration
+    # Apply sharding configuration
     sleep 15
     docker exec -it "${COMPOSE_PROJECT_NAME}-${ENV}-router-01-1" sh -c "mongosh --port 27017 < /mongo-sharding.init.js"
 
-    ## Enable admin account
+    # Enable admin account
     sleep 5
     docker exec -it "${COMPOSE_PROJECT_NAME}-${ENV}-router-01-1" sh -c "mongosh --port 27017 < /mongo-auth.init.js"
-    # ...
 else
-    # --init seçeneği verilmediğinde yapılacak işlemler
-    echo "Volume'leri silinmemiş DB'yi tekrar ayağa kaldırma işlemi yapılıyor..."
+    # Operations when the --init option is not provided
+    echo "Starting the previously deployed DB without deleting volumes..."
     # Start the whole stack
     docker-compose --file "docker-compose-${ENV}.yaml" up -d
 
-    echo "Process in progress wait! (about 1 minute)"
-    ## Enable admin account
+    echo "Process in progress. Please wait! (about 1 minute)"
+    # Enable admin account
     sleep 59
     docker exec -it "${COMPOSE_PROJECT_NAME}-${ENV}-router-01-1" sh -c "mongosh --port 27017 < /mongo-auth.js"
-    # ...
 fi
 
 echo "Process finished."
